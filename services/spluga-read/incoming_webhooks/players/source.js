c58e736d4ad02aa0fd29591f5d78c17f0ab87f94
Roberto Dell'Oro <roberto.delloro@mondora.com>
2019-07-18 14:35:21 +0200

    remove services

--- services/spluga-read/incoming_webhooks/players/source.js
+++
@@ -1,6 +0,0 @@
-exports = function(payload, response) {
-    var { id } = payload.query;
-    var result = context.services.get("mongodb-atlas").db("spluga").collection("players");
-    var query = id ? { "_id": BSON.ObjectId(id) } : {};
-    return result.find(query).toArray();
-};
